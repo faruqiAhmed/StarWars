516cfba5bd3f947c3a07edc380afffc7f7a28a58
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2024-04-23 22:35:13 +0600

    Initial commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Md Omar Faruq on 23/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
